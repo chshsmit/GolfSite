2b2c7417d683e6d1e951204a0a9d7c39de0a413c
Christopher Smith <chshsmit@gmail.com>
2020-09-19 16:43:09 -0700

    Removed unused imports

--- client/src/components/PreviousRoundCard/PreviousRoundCard.tsx
+++ client/src/components/PreviousRoundCard/PreviousRoundCard.tsx
@@ -3,23 +3,15 @@
  * @author Christopher Smith
  * @description Card to show information for a previous round
  * @created 2020-09-19T10:22:42.318Z-07:00
- * @last-modified 2020-09-19T16:41:33.263Z-07:00
+ * @last-modified 2020-09-19T16:42:51.831Z-07:00
  */
 
 //---------------------------------------------------------------------------------------------------
 
 import React, { useEffect, useState } from "react";
-import clsx from "clsx";
 
-import {
-  Card,
-  IconButton,
-  CircularProgress,
-  CardActions,
-} from "@material-ui/core";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-
+import { Card, CircularProgress } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import { BaseRound, SingleRoundData } from "types/ApiResponseTypes";
 
 import PreviousRoundCardHeader from "components/PreviousRoundCard/PreviousRoundCardHeader";
@@ -29,23 +21,11 @@ import { makeCamelCase } from "utils/utils";
 
 //---------------------------------------------------------------------------------------------------
 
-const previousRoundCardStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      minWidth: "20%",
-    },
-    expand: {
-      transform: "rotate(0deg)",
-      marginLeft: "auto",
-      transition: theme.transitions.create("transform", {
-        duration: theme.transitions.duration.shortest,
-      }),
-    },
-    expandOpen: {
-      transform: "rotate(180deg)",
-    },
-  })
-);
+const previousRoundCardStyles = makeStyles({
+  root: {
+    minWidth: "20%",
+  },
+});
 
 //---------------------------------------------------------------------------------------------------
 
@@ -58,11 +38,6 @@ const PreviousRoundCard = ({
   const classes = previousRoundCardStyles();
   const [roundData, setRoundData] = useState<SingleRoundData | null>(null);
 
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
   useEffect(() => {
     fetch(
       `http://127.0.0.1:5000/sheets/roundData/${makeCamelCase(
@@ -74,8 +49,6 @@ const PreviousRoundCard = ({
         setRoundData(response);
       });
   }, []);
-
-  console.log(expanded);
 
   return roundData ? (
     <Card className={classes.root}>
