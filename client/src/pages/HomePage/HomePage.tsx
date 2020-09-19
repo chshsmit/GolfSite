b0230102d6b173ac96117f513dc354b5250949be
Christopher Smith <chshsmit@gmail.com>
2020-09-18 17:48:24 -0700

    Moved around some locations and finished up routing

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -3,48 +3,17 @@
  * @author Christopher Smith
  * @description
  * @created 2020-09-17T21:41:29.992Z-07:00
- * @last-modified 2020-09-18T17:38:42.990Z-07:00
+ * @last-modified 2020-09-18T17:46:41.705Z-07:00
  */
 
-import React, { useState, useEffect } from "react";
-import { Grid } from "@material-ui/core";
-
-import GraphOptionSelect from "components/Graph/GraphOptionSelect";
+import React from "react";
 import MainNavBar from "components/MainNavBar/MainNavBar";
-import Graph from "components/Graph/Graph";
-import "pages/HomePage/HomePage.css";
 
 const HomePage = (): React.ReactElement => {
-  const [graphOptions, setGraphOptions] = useState([]);
-  const [graphOption, setGraphOption] = useState("");
-
-  useEffect(() => {
-    fetch("http://127.0.0.1:5000/options/graph")
-      .then((res) => res.json())
-      .then((response) => setGraphOptions(response));
-  }, []);
-
   return (
     <>
       <MainNavBar currentScreen="Home" />
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-        spacing={2}
-      >
-        <Grid item>
-          <GraphOptionSelect
-            value={graphOption}
-            onChange={setGraphOption}
-            graphOptions={graphOptions}
-          />
-        </Grid>
-        <Grid item>
-          <Graph graphOption={graphOption} />
-        </Grid>
-      </Grid>
+      <h1>HomePage</h1>
     </>
   );
 };
--- client/src/pages/PlayData/PlayData.tsx
+++ client/src/pages/PlayData/PlayData.tsx
@@ -4,15 +4,49 @@
  * @description
  * @created 2020-09-18T17:40:43.966Z-07:00
  * @copyright
- * @last-modified 2020-09-18T17:41:42.929Z-07:00
+ * @last-modified 2020-09-18T17:45:30.882Z-07:00
  */
 
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Grid } from "@material-ui/core";
 
+import GraphOptionSelect from "components/Graph/GraphOptionSelect";
 import MainNavBar from "components/MainNavBar/MainNavBar";
+import Graph from "components/Graph/Graph";
 
 const PlayData = (): React.ReactElement => {
-  return <MainNavBar currentScreen="Play Data" />;
+  const [graphOptions, setGraphOptions] = useState([]);
+  const [graphOption, setGraphOption] = useState("");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/options/graph")
+      .then((res) => res.json())
+      .then((response) => setGraphOptions(response));
+  }, []);
+
+  return (
+    <>
+      <MainNavBar currentScreen="Play Data" />
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+        spacing={2}
+      >
+        <Grid item>
+          <GraphOptionSelect
+            value={graphOption}
+            onChange={setGraphOption}
+            graphOptions={graphOptions}
+          />
+        </Grid>
+        <Grid item>
+          <Graph graphOption={graphOption} />
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default PlayData;
